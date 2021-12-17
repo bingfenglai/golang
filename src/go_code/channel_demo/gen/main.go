1d02b456e3111f9476a7592355e80774f8281938
赖柄沣 <bingfengdev@aliyun.com>
2021-12-17 23:26:30 +0800

    feat: 惰性生成器实现

--- src/go_code/channel_demo/gen/main.go
+++ src/go_code/channel_demo/gen/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	fmt "fmt"
+	"sync"
+)
+
+type Interval struct {
+	maxValue    int
+	currentVale int
+	step        int
+	sync.Locker
+}
+
+func (receiver *Interval) getNextInterval() (min, max int) {
+	receiver.Lock()
+	defer receiver.Unlock()
+	min = receiver.currentVale
+	max = receiver.currentVale + receiver.step
+	return min, max
+}
+
+var resume chan int
+
+func intergers() chan int {
+	yeild := make(chan int)
+	count := 0
+
+	go func() {
+		for {
+			yeild <- count
+			count++
+
+		}
+	}()
+
+	return yeild
+}
+
+func generateInteger() int {
+	return <-resume
+}
+
 func main() {
+	resume = intergers()
+
+	s := fmt.Sprintf("%05d", 3)
+	fmt.Println(s)
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
 
 }
