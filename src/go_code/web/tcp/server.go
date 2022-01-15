27416b870d48ce720610b366f3e0b5ac4d4cd8ef
赖柄沣 <bingfengdev@aliyun.com>
2022-01-15 16:57:33 +0800

    feat: tcp server

--- src/go_code/web/tcp/server.go
+++ src/go_code/web/tcp/server.go
@@ -1,4 +1,46 @@
 package main
 
+import "net"
 
+func main() {
 
+	listen, err := net.Listen("tcp", "localhost:9527")
+
+	if err != nil {
+		println("监听端口出错", err)
+		return
+
+	}
+
+	for {
+
+		accept, err := listen.Accept()
+		if err != nil {
+			println(err)
+			return
+
+		}
+
+		go doServerStuff(accept)
+
+	}
+
+}
+
+func doServerStuff(conn net.Conn) {
+
+	for {
+		buf := make([]byte, 1024)
+
+		read, err := conn.Read(buf)
+
+		if err != nil {
+			println(err)
+			return
+
+		}
+
+		println("接收到数据：", string(buf[:read]))
+	}
+
+}
